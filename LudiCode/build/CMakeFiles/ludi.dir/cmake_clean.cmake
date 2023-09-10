a398d53337054f35a6f65342eb2d6819cf108924
Aderox <aderoxgame@gmail.com>
2023-09-10 19:39:39 +0200

    Mis en place du projet

---
+++ LudiCode/build/CMakeFiles/ludi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ludi.dir/src/main.cpp.o"
+  "CMakeFiles/ludi.dir/src/main.cpp.o.d"
+  "ludi"
+  "ludi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ludi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
